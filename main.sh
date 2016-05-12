7c8182bab9a7c315dd8bf1033be27164c5c99ea2
Nick The Sick <nperez0111@gmail.com>
2016-05-12 17:59:46 -0400

    Added output directory configuration and made flat into group

--- main.sh
+++ main.sh
@@ -1,13 +1,19 @@
  #!/bin/bash 
 STR=".$1"
+output="output"
 
 if [ "$STR" == "." ]
 	then
 	STR=".png"
 fi
 
+if [ -d "$output" ]; then
+  rm -r "$output"
+fi
+mkdir "$output"
+
 for i in *$STR; do 
 	convert "$i" -background white -flatten ${i%.*}.bmp
-	potrace -s ${i%.*}.bmp -o ${i%.*}.svg --flat --progress
+	potrace -s ${i%.*}.bmp -o $output/${i%.*}.svg --group --progress
 	rm ${i%.*}.bmp
 done
