846b67c6e9df7ffbb8464b7f101fdff8f2b7fe7d
Nick The Sick <nperez0111@gmail.com>
2016-05-12 10:35:22 -0400

    Add pngtosvg shorthand added ability to convert other formats

--- main.sh
+++ main.sh
@@ -1,2 +1,13 @@
-for i in *.png; do convert "$i" -background white -flatten ${i%.*}.bmp; done
-for i in *.bmp; do potrace -s "$i" -o ${i%.*}.svg --flat --progress; done
+ #!/bin/bash 
+STR=".$1"
+
+if [ "$STR" == "." ]
+	then
+	STR=".png"
+fi
+
+for i in *$STR; do 
+	convert "$i" -background white -flatten ${i%.*}.bmp
+	potrace -s ${i%.*}.bmp -o ${i%.*}.svg --flat --progress
+	rm ${i%.*}.bmp
+done
